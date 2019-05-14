d6eaffb81e296e4cf2c34e2c102b63ea9cd97a05
TechGyal <dev.techgyal@gmail.com>
2019-05-14 03:19:09 -0700

    my second commit of the system

--- databaseConnection.php
+++ databaseConnection.php
@@ -6,7 +6,7 @@
 
 
 $host = "localhost";//define the host to the database for our case is localhost
-$user = "root";//define the user who own the database or has privilege to access it
+$user = "root";//define the user who own the database or has privilege to access it thats his or her role
 $password = "";//define the password to access the database for our case we have none
 $database_name = "salary";//Here we give the name of our database we want to connect to
 
